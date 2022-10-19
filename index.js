6706f22a542a4246105ce42d588438a7e31d5157
Lory-Stan TANASI <stantanasi@gmail.com>
2022-10-19 10:37:00 +0200

    feat(index): connect to MongoDB database

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+app.use((req, res, next) => {
+    mongoose.connect(process.env.MONGO_DB_URI)
+        .then(() => next())
+        .catch(err => next(err))
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
