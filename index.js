d29b9ef7e357ff45378512f87a47b269ff3af57a
Lory-Stan TANASI <stantanasi@gmail.com>
2022-10-19 10:37:50 +0200

    refactor: change indentation spaces

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 const app = express()
 
 app.use((req, res, next) => {
-    mongoose.connect(process.env.MONGO_DB_URI)
-        .then(() => next())
-        .catch(err => next(err))
+  mongoose.connect(process.env.MONGO_DB_URI)
+    .then(() => next())
+    .catch(err => next(err))
 });
 
 app.get('/', (req, res) => {
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
 
 const port = +(process.env.PORT || 3000)
 app.listen(port, () => {
-    console.log(`Server is listening on ${port}`)
+  console.log(`Server is listening on ${port}`)
 })
