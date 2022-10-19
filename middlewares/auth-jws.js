682488989fa366cc50273221ab8a6c442c1b8848
Lory-Stan TANASI <stantanasi@gmail.com>
2022-10-19 18:08:17 +0200

    feat(auth-jws): create isAdmin middleware using JWT

--- middlewares/auth-jws.js
+++ middlewares/auth-jws.js
@@ -16,8 +16,25 @@ const verifyToken = (req, res, next) => {
   });
 }
 
+const isAdmin = (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(403).send({ message: 'No token provided!' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err || !decoded.isAdmin) {
+      return res.status(401).send({ message: 'Unauthorized' });
+    }
+
+    next();
+  });
+}
+
 const authJwt = {
-  verifyToken
+  verifyToken,
+  isAdmin,
 };
 
 export default authJwt;
