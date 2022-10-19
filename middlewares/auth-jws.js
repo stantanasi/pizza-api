69e23ee2c35b90120a433746dc9a68b1afb25b5e
Lory-Stan TANASI <stantanasi@gmail.com>
2022-10-19 18:07:31 +0200

    refactor(auth-jws): update error message on token expired

--- middlewares/auth-jws.js
+++ middlewares/auth-jws.js
@@ -9,7 +9,7 @@ const verifyToken = (req, res, next) => {
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: 'Unauthorized' });
+      return res.status(401).send({ message: 'Token expired!' });
     }
 
     next();
