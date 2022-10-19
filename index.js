428416f8b62c0427f89ab5806048697cb35f7b97
Lory-Stan TANASI <stantanasi@gmail.com>
2022-10-19 11:52:49 +0200

    feat(index): change message on / route

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to pizza-api! 🎉')
 })
 
 const port = +(process.env.PORT || 3000)
