314f0da166694ce8b34fe838528cd2caad5e92c1
alexandre119 <alexandre@qwest.tv>
2022-10-20 15:00:22 +0200

    doc: add doc for root routes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import userRoutes from './routes/user.routes.js';
 import clientRoutes from './routes/client.routes.js';
 import employeeRoutes from './routes/employee.routes.js';
 import clientController from './controllers/client.controller.js';
+import employeeController from './controllers/employee.controller.js';
 import morgan from 'morgan'
 
 dotenv.config()
@@ -35,12 +36,46 @@ app.use((req, res, next) => {
     .catch(err => next(err))
 });
 
+/**
+ * @api {get} / Welcome Page
+ * @apiName Welcome page
+ * @apiGroup Root
+ *
+ * @apiSuccess {String} message Welcome message
+ */
 app.get('/', (req, res) => {
   res.send('Welcome to pizza-api! 🎉')
 })
 
+/**
+ * @api {post} /login Login as a user
+ * @apiName Login user
+ * @apiGroup Root
+ *
+ * @apiSuccess {String} email Email of the user
+ * @apiSuccess {String} password Password of the user
+ */
 app.post('/login', userController.login)
+
+/**
+ * @api {post} /login Login as a client
+ * @apiName Login client
+ * @apiGroup Root
+ *
+ * @apiSuccess {String} email Email of the client
+ * @apiSuccess {String} password Password of the client
+ */
 app.post('/client/login', clientController.login)
+
+/**
+ * @api {post} /login Login as an employee
+ * @apiName Login employee
+ * @apiGroup Root
+ *
+ * @apiSuccess {String} email Email of the employee
+ * @apiSuccess {String} password Password of the employee
+ */
+app.post('/employee/login', employeeController.login)
 
 app.use('/pizzas', pizzaRoutes)
 app.use('/orders', orderRoutes)
