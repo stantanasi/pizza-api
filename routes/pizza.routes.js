be72b8a855e5087ea7dec4d588e50409eb4961d3
Bouteille158 <alexandre.benali@estiam.com>
2022-10-20 16:18:49 +0200

    doc: add doc for pizza routes

--- routes/pizza.routes.js
+++ routes/pizza.routes.js
@@ -4,14 +4,65 @@ import authJwt from "../middlewares/auth-jws.js"
 
 const pizzaRoutes = Router()
 
+/**
+ * @api {get} /pizzas Get list of pizzas
+ * @apiName Get Pizzas
+ * @apiGroup Pizza
+ *
+ * @apiSuccess {String} id Id of the Pizza.
+ * @apiSuccess {String} name Name of the Pizza.
+ * @apiSuccess {number} price  Price of the Pizza.
+ */
 pizzaRoutes.get('/', [authJwt.verifyToken], pizzaController.getPizzas)
 
+/**
+ * @api {post} /pizzas Create a pizza
+ * @apiName Create Pizza
+ * @apiGroup Pizza
+ *
+ * @apiSuccess {String} id Id of the Pizza.
+ * @apiSuccess {String} name Name of the Pizza.
+ * @apiSuccess {number} price  Price of the Pizza.
+ */
 pizzaRoutes.post('/', [authJwt.verifyToken, authJwt.isAdmin], pizzaController.createPizza)
 
+/**
+ * @api {get} /pizzas/:id Get one pizza
+ * @apiName Get Pizza
+ * @apiGroup Pizza
+ * 
+ * @apiParam {String} id Pizza id
+ *
+ * @apiSuccess {String} id Id of the Pizza.
+ * @apiSuccess {String} name Name of the Pizza.
+ * @apiSuccess {number} price  Price of the Pizza.
+ */
 pizzaRoutes.get('/:id', [authJwt.verifyToken], pizzaController.getPizzaById)
 
+/**
+ * @api {patch} /pizzas/:id Update a pizza
+ * @apiName Update Pizza
+ * @apiGroup Pizza
+ * 
+ * @apiParam {String} id Pizza id
+ *
+ * @apiSuccess {String} id Id of the Pizza.
+ * @apiSuccess {String} name Name of the Pizza.
+ * @apiSuccess {number} price  Price of the Pizza.
+ */
 pizzaRoutes.patch('/:id', [authJwt.verifyToken, authJwt.isAdmin], pizzaController.updatePizzaById)
 
+/**
+ * @api {delete} /pizzas/:id Delete a pizza
+ * @apiName Delete Pizza
+ * @apiGroup Pizza
+ * 
+ * @apiParam {String} id Pizza id
+
+ * 
+ * @apiSuccess {String} message Deletion message.
+ * @apiError {String} error Pizza doesn't exist!
+ */
 pizzaRoutes.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], pizzaController.deletePizzaById)
 
 export default pizzaRoutes
